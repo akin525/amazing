fc1dbd8044714c1f41d0ce45ec9588dfd2a45a8c
akinl <akinlabisamson15@gmail.com>
2023-07-01 01:19:11 +0100

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,54 +48,54 @@
 
 
 
-<!-- ====== start top navbar ====== -->
-<div class="top-navbar style-1">
-    <div class="container">
-        <div class="content">
-            <div class="row align-items-center">
-                <div class="col-lg-8">
-                    <div class="top-links">
-                        <div class="text text-white">
-                            <i class="fas fa-bullhorn"></i>
-                            <strong>Welcome:</strong>
-                            <span>To Amazing Mobile Data/Airtime<a href="#" class="p-0"><u>1st Line IT Support
-                                            </u></a></span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="r-side">
-                        <div class="socail-icons">
-                            <a href="#">
-                                <i class="fab fa-twitter"></i>
-                            </a>
-                            <a href="#">
-                                <i class="fab fa-facebook-f"></i>
-                            </a>
-                            <a href="#">
-                                <i class="fab fa-linkedin-in"></i>
-                            </a>
-                            <a href="#">
-                                <i class="fab fa-instagram"></i>
-                            </a>
-                        </div>
-                        <div class="dropdown">
-                            <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                               data-bs-toggle="dropdown" aria-expanded="false">
-                                <img class="me-1" src="assets/img/lang.png" alt=""> English
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                <li><a class="dropdown-item" href="#">French</a></li>
-                                <li><a class="dropdown-item" href="#">Arabic</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- ====== end top navbar ====== -->
+{{--<!-- ====== start top navbar ====== -->--}}
+{{--<div class="top-navbar style-1">--}}
+{{--    <div class="container">--}}
+{{--        <div class="content">--}}
+{{--            <div class="row align-items-center">--}}
+{{--                <div class="col-lg-8">--}}
+{{--                    <div class="top-links">--}}
+{{--                        <div class="text text-white">--}}
+{{--                            <i class="fas fa-bullhorn"></i>--}}
+{{--                            <strong>Welcome:</strong>--}}
+{{--                            <span>To Amazing Mobile Data/Airtime<a href="#" class="p-0"><u>1st Line IT Support--}}
+{{--                                            </u></a></span>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--                <div class="col-lg-4">--}}
+{{--                    <div class="r-side">--}}
+{{--                        <div class="socail-icons">--}}
+{{--                            <a href="#">--}}
+{{--                                <i class="fab fa-twitter"></i>--}}
+{{--                            </a>--}}
+{{--                            <a href="#">--}}
+{{--                                <i class="fab fa-facebook-f"></i>--}}
+{{--                            </a>--}}
+{{--                            <a href="#">--}}
+{{--                                <i class="fab fa-linkedin-in"></i>--}}
+{{--                            </a>--}}
+{{--                            <a href="#">--}}
+{{--                                <i class="fab fa-instagram"></i>--}}
+{{--                            </a>--}}
+{{--                        </div>--}}
+{{--                        <div class="dropdown">--}}
+{{--                            <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"--}}
+{{--                               data-bs-toggle="dropdown" aria-expanded="false">--}}
+{{--                                <img class="me-1" src="assets/img/lang.png" alt=""> English--}}
+{{--                            </a>--}}
+{{--                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">--}}
+{{--                                <li><a class="dropdown-item" href="#">French</a></li>--}}
+{{--                                <li><a class="dropdown-item" href="#">Arabic</a></li>--}}
+{{--                            </ul>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
+{{--<!-- ====== end top navbar ====== -->--}}
 
 <!-- ====== start navbar ====== -->
 <nav class="navbar navbar-expand-lg navbar-light style-1">
