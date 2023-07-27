5dceb4f7155f53390b6bb2fdfde671702ca44b31
ACCOUNTS <akinlabisamson15@gmail.com>
2023-07-27 15:50:40 +0100

    waecpin

--- routes/web.php
+++ routes/web.php
@@ -131,6 +131,10 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/transactions', [TransactionController::class, 'getTransactions']);
     Route::get('/transactions1', [TransactionController::class, 'getTransactions1']);
 
+
+    Route::get('waecpin/{id}', [EducationController::class, 'waecpdfview'])->name('waecpin');
+    Route::get('waecpin1/{id}', [EducationController::class, 'waecpdfdownload'])->name('waecpin1');
+
 });
 
 Route::get('/logout', function(){
